d9995b2e036aea7cf253774c89ad39049fb8e424
ThePotatoDev <potatocoding@gmail.com>
2024-06-10 11:27:47 -0500

    Folia support

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -4,8 +4,10 @@ version = rootProject.version
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("com.github.ben-manes.caffeine:caffeine:3.0.5")
-
     implementation("com.willfp:ecomponent:1.4.1")
+    implementation("com.github.Anon8281:UniversalScheduler:0.1.6") {
+        isTransitive = true
+    }
 }
 
 publishing {
